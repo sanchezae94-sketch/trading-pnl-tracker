ea38b72f1ebca16fe90ba638d18f397175f4531f
sanchezae94-sketch <sanchez.ae94@gmail.com>
2026-05-29 10:21:56 -0700

    Create App component with SecurePnLTracker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SecurePnLTracker from './SecurePnLTracker';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SecurePnLTracker />
+    </div>
+  );
+}
+
+export default App;
